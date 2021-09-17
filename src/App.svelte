983b58e202762bc4e4098d2cafc2eeb2050a1573
elbertwang3 <31757115+elbertwang3@users.noreply.github.com>
2021-09-17 01:20:36 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { LayerCake, Svg } from "layercake";
+	import { data } from "./data/data";
+
+  
+  </script>
+  
+  <main>
+	<div class="chart-container">
+	  <!-- <LayerCake
+		data={grouped}
+		padding={{ top: 0, right: 0, bottom: 0, left: 0 }}
+	  >
+		<Svg>
+		  <Treemap bind:hovered {formatDollars} {rootHeight} />
+		</Svg>
+		<Tooltip {hovered} {formatDollars} />
+	  </LayerCake> -->
+	</div>
+  </main>
+  
+  <style>
+  
+	main {
+	  width: 100%;
+	  height: 100%;
+	}
+  
+	.chart-container {
+	  width: 100%;
+	  height: 100%;
+	}
+  </style>
+  
